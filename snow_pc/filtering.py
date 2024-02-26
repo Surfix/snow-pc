98ba5cc3e9edc99938cda50bb4cecac609a5d1d5
Naheem Adebisi <adebisi.naheem@gmail.com>
2024-02-26 11:11:03 -0700

    Update dem_filtering parameters and add new filtering functions

--- snow_pc/filtering.py
+++ snow_pc/filtering.py
@@ -4,7 +4,7 @@ from os.path import dirname, join
 import json
 import subprocess
 
-def dem_filtering(laz_fp, dem_fp = '', dem_low = 25, dem_high = 35):
+def dem_filtering(laz_fp, dem_fp = '', dem_low = 20, dem_high = 50):
     """Use filters.dem to filter the point cloud to the DEM."""
     #download dem using download_dem() if dem_fp is not provided
     if dem_fp == '':
@@ -40,9 +40,118 @@ def dem_filtering(laz_fp, dem_fp = '', dem_low = 25, dem_high = 35):
     with open(json_to_use, 'w') as f:
         json.dump(json_pipeline, f)
     #run the json pipeline
+    subprocess.run(["pdal", "pipeline", json_to_use], shell=True)
+
+    return out_fp
+
+def return_filtering(laz_fp):
+    """Use filters.range to filter the point cloud ......"""
+    #get the directory of the file
+    results_dir = dirname(laz_fp)
+    #create a filepath for the output las file
+    out_fp = join(results_dir, "returns_filtered.laz")
+    #create a json pipeline for pdal
+    json_pipeline = {
+        "pipeline": [
+            {
+                "type": "readers.las",
+                "filename": laz_fp
+            },
+            {
+                "type": "filters.mongo",\
+                "expression": {"$and": [\
+                {"ReturnNumber": {"$gt": 0}},\
+                {"NumberOfReturns": {"$gt": 0}} ] }
+            },
+            {
+                "type": "writers.las",
+                "filename": out_fp
+            }
+        ]
+    }
+    #create a directory to save the json pipeline
+    json_dir =  join(results_dir, 'jsons')
+    os.makedirs(json_dir, exist_ok= True)
+    json_name = 'return_filtering'
+    json_to_use = join(json_dir, f'{json_name}.json')
+    #write json pipeline to file
+    with open(json_to_use, 'w') as f:
+        json.dump(json_pipeline, f)
+    #run the json pipeline
     subprocess.run(["pdal", "pipeline", json_to_use])
 
     return out_fp
 
+def elm_filtering(laz_fp):
+    """Use filters.elm to filter low points as noise"""
+    #get the directory of the file
+    results_dir = dirname(laz_fp)
+    #create a filepath for the output las file
+    out_fp = join(results_dir, "elm_filtered.laz")
+    #create a json pipeline for pdal
+    json_pipeline = {
+        "pipeline": [
+            {
+                "type": "readers.las",
+                "filename": laz_fp
+            },
+            {
+                "type": "filters.elm"
+            },
+            {
+                "type": "writers.las",
+                "filename": out_fp
+            }
+        ]
+    }
+    #create a directory to save the json pipeline
+    json_dir =  join(results_dir, 'jsons')
+    os.makedirs(json_dir, exist_ok= True)
+    json_name = 'elm_filtering'
+    json_to_use = join(json_dir, f'{json_name}.json')
+    #write json pipeline to file
+    with open(json_to_use, 'w') as f:
+        json.dump(json_pipeline, f)
+    #run the json pipeline
+    subprocess.run(["pdal", "pipeline", json_to_use])
 
+    return out_fp
+
+def outlier_filtering(laz_fp, mean_k = 20, multiplier = 3):
+    """Use filters.outlier to filter out noise"""
+    #get the directory of the file
+    results_dir = dirname(laz_fp)
+    #create a filepath for the output las file
+    out_fp = join(results_dir, "outlier_filtered.laz")
+    #create a json pipeline for pdal
+    json_pipeline = {
+        "pipeline": [
+            {
+                "type": "readers.las",
+                "filename": laz_fp
+            },
+            {
+                "type": "filters.outlier",\
+                "method": "statistical",\
+                "mean_k": mean_k,\
+                "multiplier": multiplier
+            },
+            {
+                "type": "writers.las",
+                "filename": out_fp
+            }
+        ]
+    }
+    #create a directory to save the json pipeline
+    json_dir =  join(results_dir, 'jsons')
+    os.makedirs(json_dir, exist_ok= True)
+    json_name = 'outlier_filtering'
+    json_to_use = join(json_dir, f'{json_name}.json')
+    #write json pipeline to file
+    with open(json_to_use, 'w') as f:
+        json.dump(json_pipeline, f)
+    #run the json pipeline
+    subprocess.run(["pdal", "pipeline", json_to_use])
+
+    return out_fp
 
