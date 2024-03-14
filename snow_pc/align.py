106a4c43049823b49946ef8918ea6e105fb435ae
surfix <adebisi.naheem@gmail.com>
2024-03-13 18:54:15 -0600

    completed the point cloud clipping function

--- snow_pc/align.py
+++ snow_pc/align.py
@@ -1,13 +1,14 @@
 import os
 import json
 import subprocess
+import geopandas as gpd
 from os.path import join, dirname, exists
 
-def clip_pc(in_laz, buff_shp, dem_is_geoid, is_canopy = False):
+def clip_pc(laz_fp, align_shp, buffer_width = 3):
     """Clip the point cloud to a shapefile.
 
     Args:
-        in_laz (_type_): _description_
+        laz_fp (_type_): _description_
         buff_shp (_type_): _description_
         dem_is_geoid (_type_): _description_
         is_canopy (bool, optional): _description_. Defaults to False.
@@ -15,37 +16,46 @@ def clip_pc(in_laz, buff_shp, dem_is_geoid, is_canopy = False):
     Raises:
         Exception: _description_
     """
+    #set the working directory
+    in_dir = os.path.dirname(laz_fp)
+    os.chdir(in_dir)
 
-    if is_canopy is False:
-        clipped_pc = join(dirname(in_laz), 'clipped_pc.laz')
-        json_fp = join(dirname(in_laz), 'jsons', 'clip_align.json')
+    #create a buffer around the shapefile to clip the point cloud
+    gdf = gpd.read_file(align_shp)
+    gdf['geometry'] = gdf.geometry.buffer(buffer_width / 2) #The buffer_width is the entire width. So, must divide by 2 here to get the right distance from centerline.
+    gdf['CLS'] = 42 # Create a new attribute to be used for PDAL clip/overlay
+    buff_shp = join(in_dir, 'buffered_area.shp')
+    gdf.to_file(buff_shp)
 
-        # create json pipeline for PDAL clip
-            # Create .json file for PDAL clip
-        json_pipeline = {
-            "pipeline": [
-                in_laz,
-                {
-                    "type":"filters.overlay",
-                    "dimension":"Classification",
-                    "datasource":buff_shp,
-                    "layer":"buffered_area",
-                    "column":"CLS"
-                },
-                {
-                    "type":"filters.range",
-                    "limits":"Classification[42:42]"
-                },
-                clipped_pc
-            ]
-        }
-        with open(json_fp,'w') as outfile:
-            json.dump(json_pipeline, outfile, indent = 2)
+    clipped_pc = join(in_dir, 'clipped_pc.laz')
+    json_fp = join(in_dir, 'jsons', 'clip_align.json')
 
-        subprocess.run(['pdal', 'pipeline', json_fp])               
 
-        # Check to see if output clipped point cloud was created
-        if not exists(clipped_pc):
-            raise Exception('Output point cloud not created')
+    # Create .json file for PDAL clip
+    json_pipeline = {
+        "pipeline": [
+            laz_fp,
+            {
+                "type":"filters.overlay",
+                "dimension":"Classification",
+                "datasource":buff_shp,
+                "layer":"buffered_area",
+                "column":"CLS"
+            },
+            {
+                "type":"filters.range",
+                "limits":"Classification[42:42]"
+            },
+            clipped_pc
+        ]
+    }
+    with open(json_fp,'w') as outfile:
+        json.dump(json_pipeline, outfile, indent = 2)
 
-        print('Point cloud clipped to area')
+    subprocess.run(['pdal', 'pipeline', json_fp])               
+
+    # Check to see if output clipped point cloud was created
+    if not exists(clipped_pc):
+        raise Exception('Output point cloud not created')
+
+    print('Point cloud clipped to area')
